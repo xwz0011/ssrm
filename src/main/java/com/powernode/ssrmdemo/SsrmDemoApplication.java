8865862f879b998fc1a8ddec6765c58a75d4122d
xwz <ftqxwz@gmail.com>
2023-05-30 14:17:19 +0800

    init

---
+++ src/main/java/com/powernode/ssrmdemo/SsrmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.powernode.ssrmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsrmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsrmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powernode/ssrmdemo/SsrmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powernode.ssrmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsrmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
